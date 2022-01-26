02a59c3471b3e8100bd1412bcbea27963f73eec0
Omar Ragheb <ahmed.raph29@gmail.com>
2022-01-26 19:49:23 +0200

    removing comments

--- app.js
+++ app.js
@@ -10,27 +10,6 @@ window.addEventListener('scroll', () => {
   navbar.classList.remove('nav-toggle');
 });
 
-// See project
-// document.getElementById('button').addEventListener('click', () => {
-//   document.querySelector('.bg-modal').style.display = 'flex';
-// });
-
-// document.querySelector('.close-button').addEventListener('click', () => {
-//   document.querySelector('.bg-modal').style.display = 'none';
-// });
-
-// document.getElementById('button-1').addEventListener('click', () => {
-//   document.querySelector('.bg-modal').style.display = 'flex';
-// });
-
-// document.getElementById('button-2').addEventListener('click', () => {
-//   document.querySelector('.bg-modal').style.display = 'flex';
-// });
-
-// document.getElementById('button-3').addEventListener('click', () => {
-//   document.querySelector('.bg-modal').style.display = 'flex';
-// });
-
 const cards = [
   {
     title: 'Tonic',
