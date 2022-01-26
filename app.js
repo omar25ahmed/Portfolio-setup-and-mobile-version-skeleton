40ffe538604023d6c1095a2c22aa784f123739b3
Omar Ragheb <ahmed.raph29@gmail.com>
2022-01-26 03:37:17 +0200

    Applying the functionality

--- app.js
+++ app.js
@@ -9,3 +9,24 @@ window.addEventListener('scroll', () => {
   menu.classList.remove('fa-times');
   navbar.classList.remove('nav-toggle');
 });
+
+// See project
+document.getElementById('button').addEventListener('click', () => {
+  document.querySelector('.bg-modal').style.display = 'flex';
+});
+
+document.querySelector('.close-button').addEventListener('click', () => {
+  document.querySelector('.bg-modal').style.display = 'none';
+});
+
+document.getElementById('button-1').addEventListener('click', () => {
+  document.querySelector('.bg-modal').style.display = 'flex';
+});
+
+document.getElementById('button-2').addEventListener('click', () => {
+  document.querySelector('.bg-modal').style.display = 'flex';
+});
+
+document.getElementById('button-3').addEventListener('click', () => {
+  document.querySelector('.bg-modal').style.display = 'flex';
+});
